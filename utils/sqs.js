cbcda13820ce651c7833f2c04b93e494921bf319
Amoghtech <amoghnagarpersonal@gmail.com>
2024-07-07 18:29:40 +0530

    Unique Index - Done

--- utils/sqs.js
+++ utils/sqs.js
@@ -1,16 +1,13 @@
 const AWS = require("aws-sdk");
 AWS.config.update({
   region: process.env.AWS_REGION ,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 let sqs;
 let sqsSetup = {
   init: () => {
-    sqs = new AWS.SQS({
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      },
-    });
+    sqs = new AWS.SQS();
     console.log("SQS Instance Created");
   },
   getSQS: () => {
